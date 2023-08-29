bb01cc93f824ff05d3f11b7a7c1db4ac6e34eb17
Amin786 <136463850+Aminjan786@users.noreply.github.com>
2023-08-29 22:28:26 +0100

    Production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_KEY : 'bee2331420897779651499de6be43fdf',
+  API_KEY : '',
   API_URL  :  'https://api.openweathermap.org/data/2.5/',
 };
 
